b1f2f27c31160f6f4d47d9702a047dfd3f8d6e04
Soumyadip Sarkar <soumyadipsarkar@Soumyadips-MacBook-Air.local>
2025-08-26 19:46:02 -0500

    Data ingestion feature added

--- src/logger.py
+++ src/logger.py
@@ -15,9 +15,3 @@ logging.basicConfig(
 
 
 )
-
-if __name__=="__main__":
-    logging.info("Logging has started")
-    logging.info("Logging has started2")
-    logging.info("Logging has started3")
-    logging.info("Logging has started4")
